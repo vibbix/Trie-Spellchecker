c0342348219607290015adc4129b1f474d0cea93
end user <kreimendahlf@wit.edu>
2017-02-07 11:44:03 -0500

    added skeleton code

---
+++ src/edu/wit/cs/comp2350/Trie.java
@@ -0,0 +1,23 @@
+package edu.wit.cs.comp2350;
+
+public class Trie extends Speller {
+
+	@Override
+	public void insertWord(String s) {
+		// TODO Implement this method
+
+	}
+
+	@Override
+	public boolean contains(String s) {
+		// TODO Implement this method
+		return false;
+	}
+
+	@Override
+	public String[] getSugg(String s) {
+		// TODO Implement this method
+		return null;
+	}
+
+}
