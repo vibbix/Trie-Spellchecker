7acce3b02576baab2066d870bcb71f953e3fa3fe
Mark Beznos <mark.vibbix@gmail.com>
2017-02-16 20:22:22 -0500

    commented out other tests

--- src/edu/wit/cs/comp2350/tests/TestSuite.java
+++ src/edu/wit/cs/comp2350/tests/TestSuite.java
@@ -6,9 +6,9 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         LAB5TestCase.class,
-        BinTreeTestCase.class,
-        HashTableTestCase.class,
-        LinearTestCase.class,
+//        BinTreeTestCase.class,
+//        HashTableTestCase.class,
+//        LinearTestCase.class,
 })
 
 public class TestSuite {
