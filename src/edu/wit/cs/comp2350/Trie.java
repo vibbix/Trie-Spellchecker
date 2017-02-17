f7a7dbfeb94d14c400a23f372b383ec79dd4be31
Mark Beznos <mark.vibbix@gmail.com>
2017-02-16 20:59:44 -0500

    Optimized Trie class by using a SortedSet Added javadoc

--- src/edu/wit/cs/comp2350/Trie.java
+++ src/edu/wit/cs/comp2350/Trie.java
@@ -1,17 +1,26 @@
 package edu.wit.cs.comp2350;
 
-import java.util.ArrayList;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Trie extends Speller {
     private TrieNode root;
-    private int editDistance = 2;
+    private boolean sortStrings = false;
 
+    /**
+     * Creates a new Trie data structure for use in spell checking
+     */
     public Trie() {
         this.root = new TrieNode('\0', false);
     }
 
-	@Override
-	public void insertWord(String s) {
+    /**
+     * Inserts a word into the trie
+     *
+     * @param s String to add
+     */
+    @Override
+    public void insertWord(String s) {
         s = s.toLowerCase().replaceAll("\\P{Print}", "");
         //for each character
         TrieNode current = this.root;
@@ -25,8 +34,14 @@ public class Trie extends Speller {
         current.isFullWord = true;
     }
 
-	@Override
-	public boolean contains(String s) {
+    /**
+     * Checks if a string is inside the trie
+     *
+     * @param s The string to check
+     * @return True if the word is inside the trie
+     */
+    @Override
+    public boolean contains(String s) {
 		// TODO Implement this method
         s = s.toLowerCase().replaceAll("\\P{Print}", "");
         TrieNode current = this.root;
@@ -40,25 +55,42 @@ public class Trie extends Speller {
         return current.isFullWord;
     }
 
-	@Override
-	public String[] getSugg(String s) {
-        s = s.toLowerCase().replaceAll("\\P{Print}", "");
-        ArrayList<String> suggestions = new ArrayList<>();
-        ArrayList<String> results = new ArrayList<>();
-        findWords(s, suggestions, 0, this.editDistance);
-        for (String sugg : suggestions) {
-            if (this.contains(sugg))
-                results.add(sugg);
-        }
-        return results.toArray(new String[0]);
+    /**
+     * Gets suggestions for words
+     *
+     * @param s The string to check
+     * @return Array of possible values
+     */
+    @Override
+    public String[] getSugg(String s) {
+        return this.findWords(s.toLowerCase().replaceAll("\\P{Print}", "")).toArray(new String[0]);
     }
 
-    private void findWords(String s, ArrayList<String> suggestions, int strPos, int editDistance) {
+    /**
+     * A helper method for the recursive findWords method
+     *
+     * @param s String to check for
+     * @return returns an ArrayList of possible suggestions
+     */
+    private SortedSet<String> findWords(String s) {
+        SortedSet<String> suggestions = new TreeSet<>();
+        int editDistance = 2;
+        findWords(s, suggestions, 0, editDistance);
+        return suggestions;
+    }
+
+    /**
+     * Appends and automatically sorts a list of possible suggestions for a given string s
+     *
+     * @param s            String to check
+     * @param suggestions  The set of current suggestions
+     * @param strPos       Current position of
+     * @param editDistance the string distance between the modified string and the intended word
+     */
+    private void findWords(String s, SortedSet<String> suggestions, int strPos, int editDistance) {
         if (strPos == s.length()) {
-            if (this.contains(s)) {
-                if (!suggestions.contains(s))
-                    suggestions.add(s);
-            }
+            if (this.contains(s) && !suggestions.contains(s))
+                suggestions.add(s);
             return;
         }
         for (int i = 0; i < 26; i++)
@@ -69,6 +101,9 @@ public class Trie extends Speller {
                         suggestions, strPos + 1, editDistance - 1);
     }
 
+    /**
+     * A single node in the trie structure
+     */
     private class TrieNode {
         char letter;
         boolean isFullWord;
@@ -76,10 +111,11 @@ public class Trie extends Speller {
         TrieNode[] children;
 
         /**
+         * Creates a new TrieNode
          * @param letter The individual character
          * @param isWord true if all the parent characters make up a word
          */
-        public TrieNode(char letter, boolean isWord) {
+        TrieNode(char letter, boolean isWord) {
             this.letter = letter;
             this.children = new TrieNode[128]; //ascii range
             this.parent = null;
